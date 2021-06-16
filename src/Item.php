b6ac41366f7a1bdcc87b40af3d16edc8f486c25e
Stewart Walter <code@phatkoala.uk>
2021-06-16 21:25:08 +0100

    Revert changes to Item as I was too eager to impress and missed that this should not be edited in the spec

--- src/Item.php
+++ src/Item.php
@@ -1,42 +1,21 @@
 <?php
 
-declare(strict_types=1);
-
 namespace App;
 
 class Item
 {
-    /**
-     * @var string The name of the product
-     */
-    public string $name;
+    public $name;
+    public $sellIn;
+    public $quality;
 
-    /**
-     * @var int The number of days we have to sell the item
-     */
-    public int $sellIn;
-
-    /**
-     * @var int How valuable the item is
-     */
-    public int $quality;
-
-    /**
-     * @param string $name
-     * @param int $quality
-     * @param int $sellIn
-     */
-    public function __construct(string $name, int $quality, int $sellIn)
+    public function __construct($name, $quality, $sellIn)
     {
         $this->name = $name;
         $this->quality = $quality;
         $this->sellIn = $sellIn;
     }
 
-    /**
-     * @return string
-     */
-    public function __toString(): string
+    public function __toString()
     {
         return "{$this->name}, {$this->sellIn}, {$this->quality}";
     }
