bc5f70fcef4d89de0532db30ba3528dff863c8d7
Stewart Walter <code@phatkoala.uk>
2021-06-16 20:49:20 +0100

    Refactor GlidedRose.php to use abstracted items

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace App;
 
+use App\Product\ProductInterface;
+use Psr\Log\LoggerInterface;
+
 class GildedRose
 {
     /**
@@ -12,9 +15,15 @@ class GildedRose
     private array $items;
 
     /**
-     * @param Item[] $items
+     * @var LoggerInterface
      */
-    public function __construct(array $items)
+    private LoggerInterface $logger;
+
+    /**
+     * @param Item[] $items
+     * @param LoggerInterface|null $logger
+     */
+    public function __construct(array $items, LoggerInterface $logger = null)
     {
         $this->items = $items;
     }
@@ -31,52 +40,65 @@ class GildedRose
         );
     }
 
-    public function nextDay(): void
+    public function nextDay()
     {
         foreach ($this->items as $item) {
-            if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if ($item->quality > 0) {
-                    if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                        $item->quality = $item->quality - 1;
-                    }
-                }
+            if ($item instanceof ProductInterface) {
+                $item->nextDay();
             } else {
-                if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
-                    if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->sellIn < 11) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                        if ($item->sellIn < 6) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                    }
+                if (!is_null($this->logger)) {
+                    $this->logger->alert(sprintf(
+                        'The product %s (%s) does not implement the interface %s',
+                        $item->name,
+                        get_class($item),
+                        ProductInterface::class
+                    ));
                 }
             }
-            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                $item->sellIn = $item->sellIn - 1;
-            }
-            if ($item->sellIn < 0) {
-                if ($item->name != 'Aged Brie') {
-                    if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->quality > 0) {
-                            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                                $item->quality = $item->quality - 1;
-                            }
-                        }
-                    } else {
-                        $item->quality = $item->quality - $item->quality;
-                    }
-                } else {
-                    if ($item->quality < 50) {
-                        $item->quality = $item->quality + 1;
-                    }
-                }
-            }
+
+//            if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
+//                if ($item->quality > 0) {
+//                    if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+//                        $item->quality = $item->quality - 1;
+//                    }
+//                }
+//            } else {
+//                if ($item->quality < 50) {
+//                    $item->quality = $item->quality + 1;
+//                    if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
+//                        if ($item->sellIn < 11) {
+//                            if ($item->quality < 50) {
+//                                $item->quality = $item->quality + 1;
+//                            }
+//                        }
+//                        if ($item->sellIn < 6) {
+//                            if ($item->quality < 50) {
+//                                $item->quality = $item->quality + 1;
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+//                $item->sellIn = $item->sellIn - 1;
+//            }
+//            if ($item->sellIn < 0) {
+//                if ($item->name != 'Aged Brie') {
+//                    if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
+//                        if ($item->quality > 0) {
+//                            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+//                                $item->quality = $item->quality - 1;
+//                            }
+//                        }
+//                    } else {
+//                        $item->quality = $item->quality - $item->quality;
+//                    }
+//                } else {
+//                    if ($item->quality < 50) {
+//                        $item->quality = $item->quality + 1;
+//                    }
+//                }
+//            }
         }
     }
 }
