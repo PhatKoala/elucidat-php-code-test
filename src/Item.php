5436fe11b7add075a4822eeac8e4599a4def3724
Stewart Walter <code@phatkoala.uk>
2021-06-16 19:53:15 +0100

    Add comments and enforce strict types
    Adding strict types is not advisable in a legacy system (usually) but for this simple task it was safe to do so

--- src/Item.php
+++ src/Item.php
@@ -1,20 +1,41 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App;
 
 class Item
 {
-    public $name;
-    public $sellIn;
-    public $quality;
+    /**
+     * @var string The name of the product
+     */
+    public string $name;
 
-    public function __construct($name, $quality, $sellIn)
+    /**
+     * @var int The number of days we have to sell the item
+     */
+    public int $sellIn;
+
+    /**
+     * @var int How valuable the item is
+     */
+    public int $quality;
+
+    /**
+     * @param string $name
+     * @param int $quality
+     * @param int $sellIn
+     */
+    public function __construct(string $name, int $quality, int $sellIn)
     {
         $this->name = $name;
         $this->quality = $quality;
         $this->sellIn = $sellIn;
     }
 
+    /**
+     * @return string
+     */
     public function __toString()
     {
         return "{$this->name}, {$this->sellIn}, {$this->quality}";
